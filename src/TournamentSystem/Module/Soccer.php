ffc680bfa71a434647b5a101a2713f488ffc19ce
Neo Hornberger <neo.hornberger@gmx.de>
2023-03-22 21:18:07 +0100

    Changed module installation to use table generation

--- src/TournamentSystem/Module/Soccer.php
+++ src/TournamentSystem/Module/Soccer.php
@@ -8,34 +8,50 @@ use TournamentSystem\Controller\Soccer\CoachController;
 use TournamentSystem\Controller\Soccer\PlayerController;
 use TournamentSystem\Controller\Soccer\TournamentController;
 use TournamentSystem\Controller\Soccer\TournamentListController;
+use TournamentSystem\Database\Database;
+use TournamentSystem\Model\Club;
+use TournamentSystem\Model\Coach;
+use TournamentSystem\Model\Player;
+use TournamentSystem\Model\Tournament;
 
 class Soccer extends Module {
-	
+
 	public function handle(array $page): ?Controller {
 		if($page[0] === '') {
 			return new TournamentListController();
 		}
 		if($page[0] === 'player' && is_numeric($page[1])) {
 			self::set('id', $page[1]);
-			
+
 			return new PlayerController();
 		}
 		if($page[0] === 'coach' && is_numeric($page[1])) {
 			self::set('id', $page[1]);
-			
+
 			return new CoachController();
 		}
 		if($page[0] === 'club' && is_numeric($page[1])) {
 			self::set('id', $page[1]);
-			
+
 			return new ClubController();
 		}
 		if(is_numeric($page[0])) {
 			self::set('id', $page[0]);
-			
+
 			return new TournamentController();
 		}
-		
+
 		return null;
+	}
+
+	public function initDatabase(Database $db): void {
+		$db->createTable(Tournament::class);
+		$db->createTable(Player::class);
+		$db->createTable(Coach::class);
+		$db->createTable(Club::class);
+	}
+
+	public function deinitDatabase(Database $db): void {
+		// TODO: Implement deinitDatabase() method.
 	}
 }
